973605395a357dcec1907c353e1189fa149942fc
vvgomes <vinicius.vieira.gomes@gmail.com>
2026-05-09 00:39:30 -0400

    finally adding some useful remaps. gotta remember using them.

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -7,3 +7,19 @@ vim.keymap.set("n", "gb", "<C-o>")
 
 -- gB to navigate forward
 vim.keymap.set("n", "gB", "<C-i>")
+
+-- move highlighted lines
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- lock cursor when removing \n
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- preserves clipboard after pasting over hl
+vim.keymap.set("x", "<leader>p", '"_dP')
+
+-- live replace
+vim.keymap.set("n", "<leader>z", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- at least 8 lines at the bottom
+vim.opt.scrolloff = 8
